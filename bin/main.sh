5a6fb420bd66d0f48489bf1b5217d0af673360a0
Oleh Matiiuk <olgerd27@gmail.com>
2022-06-16 00:27:17 +0300

    Made independent search of 'installed' and 'available' packages

--- bin/main.sh
+++ bin/main.sh
@@ -96,24 +96,27 @@ glibc.i686                     2.34-34.fc35             updates
 glibc.x86_64                   2.34-34.fc35             updates"
 
 ####### The Main loop
-for PKG in gcc.x86_64 bison.x86_64 make.x86_64 glibc.x86_64 libXi.i686; do
+for PKG in gcc.x86_64 bison.x86_64 glibc.x86_64 libXi.i686 make.x86_64; do
   do_out "Package ${PKG}:" 12
-  # search packages in the 'installed' packages list
+
+  # Search the packages in the 'installed' packages list
   PKG_INST_FND=$(echo "$PKGS_INST" | grep -E "^${PKG}") 
   if [ $? -eq 0 ]; then
     do_out "INSTALLED" 12
     do_out "$PKG_INST_FND" 2
   else
     do_out "NOT INSTALLED" 12
-    # search packages in the 'available' packages list
-    PKG_AVLB_FND=$(echo "$PKGS_AVLB" | grep -E "^${PKG}") 
-    if [ $? -eq 0 ]; then
-      do_out "AVAILABLE" 12
-      do_out "$PKG_AVLB_FND" 2
-    else
-      do_out "NOT AVAILABLE" 12
-    fi
   fi
+
+  # Search the packages in the 'available' packages list
+  PKG_AVLB_FND=$(echo "$PKGS_AVLB" | grep -E "^${PKG}") 
+  if [ $? -eq 0 ]; then
+    do_out "AVAILABLE" 12
+    do_out "$PKG_AVLB_FND" 2
+  else
+    do_out "NOT AVAILABLE" 12
+  fi
+
   do_out "$SEP_BODY" 12
 done
 
