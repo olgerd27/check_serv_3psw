fa5daa3c5524e7caa8ea3d33897f69f38f12a6ce
Oleh Matiiuk <olgerd27@gmail.com>
2022-07-11 20:42:26 +0300

    Implemented a statistics collection

--- bin/check_pkg.sh
+++ bin/check_pkg.sh
@@ -18,30 +18,36 @@ function getItem
 # $1 - a list of packages
 # $2 - the searched package name
 # $3 - decision message
+# RC: 0 - package is found,
+#     1 - package is not found
 function findPackage
 {
   PKG_FND=$(echo "$1" | grep -E "^${2} ")
-  if [ $? -eq 0 ]; then
+  RC=$?
+  if [ $RC -eq 0 ]; then
     PKG_FND="$(echo "$PKG_FND" | sed 's/     / /g')" # decrease a number of spaces
     do_out "$3" 12
     do_out "$PKG_FND" 2
   else
     do_out "NOT $3" 12
   fi
+  return $RC
 }
 
 ################################################################
 # Execute the Command from the dat-file
 # $1 - the Command for the Package
-# $2 - a flag: True - if there is a Short Package Name;
+# $2 - a flag: True - if a Short Package Name is specified in dat-file;
 #              False - if it's not specified in dat-file
 # $3 - decision message
+# RC: 0 - package is installed,
+#     1 - package is not installed
 function ExecPkgCommand
 {
   OUT_CMD="$(eval $1 2>&1)"
   RC_CMD=$?
-  # If Command OK && package cannot be searched in package manager
-  # (short name is EMPTY), then this package is INSTALLED.
+  # If Short Package Name is EMPTY && Command executed OK, 
+  # then this package is INSTALLED.
   if [ $2 = False ]; then
     [ $RC_CMD -eq 0 ] &&
       do_out "$3" 12 ||
@@ -49,5 +55,6 @@ function ExecPkgCommand
   fi
   do_out "Command execution:" 2
   do_out "$OUT_CMD" 2
+  return $RC_CMD
 }
 
--- bin/main.sh
+++ bin/main.sh
@@ -1,4 +1,8 @@
 #!/bin/ksh
+# TODO: create the 'install' file, which can do the following:
+#       - create 'out' directory
+#       - chmod -R g+w .
+#       - chmod 774 bin/*
 ###############################################################################
 # The main program
 ###############################################################################
@@ -22,6 +26,11 @@ SH_CHECK_PKG=${D_CURR}/check_pkg.sh
 ####### Initialization
 # Init checks
 [ "$(uname -s)" != "Linux" ] && echo "Please run on Linux only" >&2 && exit 1
+# TODO: check if dat-file exists
+# TODO: check if $D_OUT dir exists, if not - print a message like:
+# The program is not installed, please check existance of $D_OUT directory.
+# TODO: move here the Report file creation if $D_OUT exists
+
 
 # Load the shell-scripts
 for SCR in $SH_FUNC_PRN $SH_CHECK_PKG; do
@@ -29,10 +38,18 @@ for SCR in $SH_FUNC_PRN $SH_CHECK_PKG; do
 done
 
 # Init the variables
+# Data from dat-file
 PKG_SHRT_NAME=  # short name
 PKG_LONG_NAME=  # long name
 PKG_BIT=        # bitness
 PKG_CMD=        # command to execute
+
+# Statistics - number of packages:
+N_ALL=0        # all
+N_INS=0        # installed
+N_NINS=0       # not installed
+N_NINS_AVL=0   # not installed & available
+N_NINS_NAVL=0  # not installed & not available
 
 ####### Start the program
 touch $FL_REP
@@ -56,8 +73,8 @@ gcc.x86_64                     11.3.1-2.fc35            updates
 glibc.i686                     2.34-35.fc35             updates                   
 glibc.x86_64                   2.34-35.fc35             updates"
 # - using yum 
-PKGS_INST="$(yum list installed)"
-PKGS_AVLB="$(yum list available)"
+#PKGS_INST="$(yum list installed)"
+#PKGS_AVLB="$(yum list available)"
 # - using rpm
 #PKGS_INST="$(rpm -qa)"
 printf "DONE\n%s\n" $SEP_BODY
@@ -73,33 +90,78 @@ while read -r LINE; do
   PKG_BIT=$(getItem "$LINE" ":" 3)
   PKG_CMD=$(getItem "$LINE" ":" 4)
 
-  # Taking decisions based on the initial data (dat-file)
-  STR_SHRT=""  # init the string with the package short name
-  CAN_FIND_PKG=False  # init flag if it's possible to find package in package manager
-  if [ -n "$PKG_SHRT_NAME" ]; then
+  # Validation and initialization by the 'Package Short Name' value from dat-file
+  if [ -z "$PKG_SHRT_NAME" ]; then
+    STR_SHRT=""  # string with the package short name
+    CAN_FIND_PKG=False  # flag if it's possible to find package in pkg manager
+  else
     STR_SHRT=" (${PKG_SHRT_NAME})"
     CAN_FIND_PKG=True
   fi
 
+  # Print a title for this package
   do_out "Package '${PKG_LONG_NAME}' ${PKG_BIT}-bit${STR_SHRT}:" 12
 
-  # FInd the package(-s) in different packages lists, and run only if the 
+  # Validation and initialization by the Command' value from dat-file
+  # TODO: add check for PKG_CMD that it's more like a command, 
+  #       e.g. check the length of the data in it
+  if [ -z "$PKG_CMD" ]; then
+    if [ $CAN_FIND_PKG = False ]; then
+      echo "!---Error 2. Invalid data in dat-file - empty short name and command" 1>&2
+      echo "Please specify at least one of these values" 1>&2
+      exit 2
+    fi
+    CAN_RUN_CMD=False
+  else
+    CAN_RUN_CMD=True
+  fi
+
+  # Find the package(-s) in different packages lists only if the 
   # the package short name is specified in the dat-file
   if [ $CAN_FIND_PKG = True ]; then
-    findPackage "$PKGS_INST" "$PKG_SHRT_NAME" "INSTALLED" #find in installed pkg list
-    findPackage "$PKGS_AVLB" "$PKG_SHRT_NAME" "AVAILABLE" #find in available pkg list
+    # TODO: rename function name - start with a capital letter 
+    # find in a list of installed packages
+    findPackage "$PKGS_INST" "$PKG_SHRT_NAME" "INSTALLED"
+    RC_INS=$?
+    # TODO: do the search of package within the available packages only if it's not 
+    # found within the installed packages
+    # find in a list of available packages
+    findPackage "$PKGS_AVLB" "$PKG_SHRT_NAME" "AVAILABLE"
+    RC_AVL=$?
   fi
 
   # Execute the Command from the dat-file
-  if [ -n "$PKG_CMD" ]; then
+  if [ $CAN_RUN_CMD = True ]; then
     ExecPkgCommand "$PKG_CMD" $CAN_FIND_PKG "INSTALLED"
+    RC_CMD=$?
+  fi
+
+  # Collection of statistics - number of packages:
+  ((N_ALL++))  # - all
+  if [[ $CAN_FIND_PKG = True && $RC_INS -eq 0 ]] || 
+     [[ $CAN_FIND_PKG = False && $RC_CMD -eq 0 ]]; then
+    ((N_INS++))       # - installed
+  else
+    ((N_NINS++))      # - not installed
+    if [[ $CAN_FIND_PKG = True && $RC_INS -ne 0 && $RC_AVL -eq 0 ]]; then
+      ((N_NINS_AVL++))  # - not installed & available
+    else
+      ((N_NINS_NAVL++)) # - not installed & not available
+    fi
   fi
 
   do_out "$SEP_BODY" 12  # print the line separator
 done < $FL_DAT
 
-# TODO: print the statistics: number of packages: all, installed, not installed, 
-#       not installed & available, not installed & not available
+# TODO: print the statistics in an external finction - number of packages: all, 
+# installed, not installed, not installed & available, not installed & not available
+echo "Package Statistics:"
+echo "  - All: ${N_ALL}"
+echo "  - Installed: ${N_INS}"
+echo "  - Not Installed: ${N_NINS}"
+echo "  - Not Installed & Available: ${N_NINS_AVL}"
+echo "  - Not Installed & Not Available: ${N_NINS_NAVL}"
+echo "$SEP_HEAD"
 
 # Print the Report file name
 do_out "The Report file:
